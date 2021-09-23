d960828aa3ee28324ff4504af591b73f618c5322
Shreyansh Agarwal <shreyansha20@iitk.ac.in>
2021-09-23 11:48:44 +0530

    Initialised the component tree

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from the navbar</h3>
+            </div>
+        )
+    }
+}
---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class product extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from the product</h3>
+            </div>
+        )
+    }
+}
