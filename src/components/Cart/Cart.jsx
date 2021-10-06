909c461533beb2ebf0ac4ddca298e5e12fe9d329
Shreyansh Agarwal <shreyansha20@iitk.ac.in>
2021-10-06 19:06:54 +0530

    Added cart list

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 import Title from '../title'
 import CartColumns from './CartColumns'
+import {ProductConsumer} from "../../context"
+import EmptyCart from './EmptyCart'
+import CartList from './CartList'
 
 export default class Cart extends Component {
     render() {
         return (
             <section>
-                <Title name="your" title="cart"/>
-                <CartColumns/>
+                <ProductConsumer>
+                    {value => {
+                        const {cart} = value;
+                        if(cart.length > 0) {
+                            return (
+                                <React.Fragment>
+                                    <Title name="your" title="cart"/>
+                                    <CartColumns/>
+                                    <CartList value={value}/>
+                                </React.Fragment>
+                            )
+                        } else{
+                            return (
+                                <EmptyCart/>
+                            )
+                        }
+
+                    }}
+                </ProductConsumer>
+                
             </section>
         )
     }
